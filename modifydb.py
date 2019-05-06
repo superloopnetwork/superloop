a3c27865ad36bbd351ad2e419129bc75c662bf28
Wai Lit Loi <wailit@gmail.com>
2019-05-06 16:27:48 +0000

    New supported feature of modifying DB via CLI

--- modifydb.py
+++ modifydb.py
@@ -1,19 +1,41 @@
 # THIS MODULE ALLOWS MODIFICATION TO BE MADE TO THE DATABASE FILE
+# APPEND FUNCTION ALLOWS USERS TO ADD DEVICE TO DATABASE WITHOUT MANUALLY OPENING UP THE YAML FILE
+# REMOVE FUNCTION DELETES A NODE BY HOSTNAME OR IP FROM THE YAML FILE
 
+from processdb import process_nodes
+from snmp import snmp
 import yaml
 
-def append(snmp_data):
+def append(args):
 
-	new_node = yaml.dump(snmp_data,default_flow_style = False)
+	argument_node = args.ip
+	device = snmp(argument_node)
+
+	new_node = yaml.dump(device,default_flow_style = False)
 	with open('nodes.yaml','a') as f:
 		f.write(new_node)
 
 	print("[+] NEW NODE SUCCESSFULLY APPENDED TO DATABASE")
 
-#def remove(hostname):
-#
-#	with open('nodes.yaml','a') as f:
-#		nodes = yaml.load(f)
-#
-#	for node in nodes:
-#		if nodes['hostname'] == hostname:         
+def remove(args):
+
+	# RE-USING PREVIOUSLY CODED FUNCTION
+	database = process_nodes()
+	# TRACK WHICH ELEMENT MATCHES
+	index = 0
+
+	for element in database:
+		if (element['hostname'] == args.argument) or (element['ip'] == args.argument):
+			break
+
+		else:
+			index = index + 1
+	# DELETES NODES FROM LIST
+	database.pop(index)
+	# WRITES TO DATABASE FILE
+	updated_database = yaml.dump(database,default_flow_style = False)
+	with open('nodes.yaml','w') as f:
+		f.write(updated_database)
+		print("[+] NEW NODE SUCCESSFULLY REMOVED FROM DATABASE")
+	
+
