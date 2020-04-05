bc4cfa095b32da3c8eb1896497ab37908873f7a6
Wai Lit Loi <wailit@gmail.com>
2020-04-05 16:29:56 -0400

    Commented print statement

--- auditdiff_engine.py
+++ auditdiff_engine.py
@@ -369,7 +369,8 @@ def juniper_audit_diff(directory,template,template_list,diff_config,edit_list,se
 
 	index_of_template_list = template_list.index(template) + 1
 
-	print("EDIT_LIST: {}".format(edit_list))
+	###UN-COMMENT THE BELOW PRINT STATEMENT FOR DEBUGING PURPOSES
+#	print("EDIT_LIST: {}".format(edit_list))
 
 	### THIS WILL CHECK IF IT'S ON THE LAST TEMPLATE. IF IT IS, IT WILL LOCATE THE LAST INDEX FOR EDIT_LIST AND APPEND IT TO THE LIST
 	if(index_of_template_list == length_template_list):
