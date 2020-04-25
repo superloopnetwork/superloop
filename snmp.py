0ed67ff13cebf99191d4b2e938ca6a652df00157
Wai Lit Loi <wailit@gmail.com>
2020-04-24 23:48:55 -0400

    Minor update in snmp.py

--- snmp.py
+++ snmp.py
@@ -3,6 +3,7 @@
 from snmp_helper import snmp_get_oid
 from snmp_helper import snmp_extract 
 from processdb import process_encrypted
+import re
 import pybase64
 import subprocess
 
@@ -15,15 +16,19 @@ def snmp(argument_node):
 	SNMP_PORT = 161
 
 	HOSTNAME_OID = '1.3.6.1.2.1.1.5.0'
+	PLATFORM_OID = '1.3.6.1.2.1.1.1.0' 
 	device = (argument_node,pybase64.b64decode(COMMUNITY_STRING),SNMP_PORT)
 	snmp_hostname = snmp_data(device,HOSTNAME_OID,SNMP_PORT)
+	snmp_platform = snmp_data(device,PLATFORM_OID,SNMP_PORT)
+
+	snmp_parse_platform(snmp_platform)
 
 	data = [{
 		'hostname': '{}'.format(snmp_hostname),
 		'ip': "{}".format(argument_node),
 		'username':'{}'.format(USERNAME_STRING),
 		'password': '{}'.format(PASSWORD_STRING),
-		'platform':'cisco',
+		'platform':'{}'.format(snmp_hostname),
 		'os':'ios',
 		'type':'ios'
 		}
@@ -37,3 +42,12 @@ def snmp_data(device,oid,port):
 	snmp_hostname = snmp_extract(snmp_data)
 
 	return snmp_hostname 
+
+def snmp_parse_platform(snmp_platform):
+
+	PLATFORM_RE = r'.+'
+	platform = re.findall(PLATFORM_RE, snmp_platform)[0]
+	
+	print("SNMP_PLATFORM: {}".format(platform))
+	
+	return platform
