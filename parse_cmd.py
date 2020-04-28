319e36262cecaab36d2352d36d543aa2725f5d2a
Wai Lit Loi <wailit@gmail.com>
2020-04-27 20:56:10 -0400

    Trying to parse out subnets from #included /path/to/file

--- parse_cmd.py
+++ parse_cmd.py
@@ -44,23 +44,30 @@ def parse_firewall_acl(node_policy,policy):
 	parse_include = f.readline()
 	path = eval(re.findall(PATH_FILTER_RE, parse_include)[0])
 	###UN-COMMENT THE BELOW PRINT STATEMENT FOR DEBUGING PURPOSES
-#	print("PATH_FILTER: {}".format(path))
+	print("PATH_FILTER: {}".format(path))
 	###UN-COMMENT THE BELOW PRINT STATEMENT FOR DEBUGING PURPOSES
 #	print("ACL_LIST inside parse_firewall_acl: {}".format(acl_list))
 	for acl in acl_list:
+
 		term = acl['term']
 		source_address = acl['source']
 		destination_address = acl['destination']
 		protocol = acl['protocol']
 		destination_port = acl['destination-port']
 		action = acl['action']
+
 		if(node_policy['platform'] == 'cisco' or node_policy['platform'] == 'juniper'):
 			print("{} {} {} {} {} {}".format(term,source_address,destination_address,protocol,destination_port,action))
 			config_list = "{} {} {} {} {} {}".format(term,source_address,destination_address,protocol,destination_port,action) 
 	
+	print
 	return config_list
 
 def parse_subnet(path):
-	
+
+	with open('{}'.format(path)) as f:
+		f.readlines()
+
+	print f
 
 	return None
