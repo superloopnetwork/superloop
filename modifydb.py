22b0f5240e74c18a89d0f6c2b4b495430fbd8557
Wai Lit Loi <wailit@gmail.com>
2021-01-11 18:44:29 -0500

    Catching exceptions. Added error handling control.

--- modifydb.py
+++ modifydb.py
@@ -24,40 +24,50 @@ def append(args):
 			break
 		else:
 			continue
-	if 'MATCH' in match_node:
-		print('+ Node currently present in database.')
-	else:
-		with open('{}/database/nodes.yaml'.format(home_directory),'a') as file:
-			file.write(new_node)
-		database = process_nodes()
-		database.sort(key=sorted)
-		updated_database = yaml.dump(database,default_flow_style = False)
-		with open('{}/database/nodes.yaml'.format(home_directory),'w') as file:
-			file.write('---\n')
-			file.write(updated_database)
-	
-		print('\u2713 SNMP discovery successful.')
-		print('+ New node appended to database.')
+	try:
+		if 'MATCH' in match_node:
+			print('+ Node currently present in database.')
+		else:
+			with open('{}/database/nodes.yaml'.format(home_directory),'a') as file:
+				file.write(new_node)
+			database = process_nodes()
+			database.sort(key=sorted)
+			updated_database = yaml.dump(database,default_flow_style = False)
+			with open('{}/database/nodes.yaml'.format(home_directory),'w') as file:
+				file.write('---\n')
+				file.write(updated_database)
+		
+			print('\u2713 SNMP discovery successful.')
+			print('+ New node appended to database.')
+	except FileNotFoundError as error:
+		print('FileNotFoundError: file cannot be found')
+		print(error)
 
 	return None
 
 def remove(args):
 	database = process_nodes()
 	index = 0
-
-	for element in database:
-		if element['hostname'] == args.argument or element['ip'] == args.argument:
-			break
-		else:
-			index = index + 1
-	"""
-		Delete node from list.
-	"""
-	database.pop(index)
-	updated_database = yaml.dump(database,default_flow_style = False)
-	with open('{}/database/nodes.yaml'.format(home_directory),'w') as file:
-		file.write('---\n')
-		file.write(updated_database)
-		print('- Node successfully removed from database.')
+	try:
+		for element in database:
+			if element['hostname'] == args.argument or element['ip'] == args.argument:
+				break
+			else:
+				index = index + 1
+		"""
+			Delete node from list.
+		"""
+		database.pop(index)
+		updated_database = yaml.dump(database,default_flow_style = False)
+		try:
+			with open('{}/database/nodes.yaml'.format(home_directory),'w') as file:
+				file.write('---\n')
+				file.write(updated_database)
+				print('- Node successfully removed from database.')
+		except FileNotFoundError as error:
+			print("FileNotFoundError: file cannot be found")
+			print(error)
+	except IndexError as error:
+		print('+ Node does not exist in database.')
 	
 	return None
