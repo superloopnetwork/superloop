72ee7837cef80c6d31080ba01cd122d539ac79e3
Wai Lit Loi <wailit@gmail.com>
2021-11-03 16:32:16 -0400

    variable name correction

--- render.py
+++ render.py
@@ -18,11 +18,11 @@ def render(template_list,node_object,auditcreeper,output,with_remediation):
 	if auditcreeper:
 	    template_list = template_list_copy[0]
 	for index in initialize.element:
-		get_hardware_vendorg_template_directory = get_template_directory(node_object[index]['hardware_vendor'],node_object[index]['opersys'],node_object[index]['type'])
+		get_hardware_vendor_template_directory = get_template_directory(node_object[index]['hardware_vendor'],node_object[index]['opersys'],node_object[index]['type'])
 		print ("{}".format(node_object[index]['name']))
 		for template in template_list:
 			config = process_jinja2_template(node_object,index,template,with_remediation)
-			print("{}{}".format(get_hardware_vendorg_template_directory,template))
+			print("{}{}".format(get_hardware_vendor_template_directory,template))
 			if(output):
 				print("{}".format(config))
 			f = open("{}/rendered-configs/{}.{}".format(get_real_path(),node_object[index]['name'],template.replace('jinja2','')) + "conf", "r")
@@ -41,9 +41,9 @@ def render(template_list,node_object,auditcreeper,output,with_remediation):
 	return None
 
 def process_jinja2_template(node_object,index,template,with_remediation):
-	get_hardware_vendorg_template_directory = get_template_directory(node_object[index]['hardware_vendor'],node_object[index]['opersys'],node_object[index]['type'])
+	get_hardware_vendor_template_directory = get_template_directory(node_object[index]['hardware_vendor'],node_object[index]['opersys'],node_object[index]['type'])
 	get_location_template_directory = get_location_directory(node_object[index]['name'],node_object[index]['hardware_vendor'],node_object[index]['type'])
-	env = Environment(loader=FileSystemLoader([get_hardware_vendorg_template_directory,get_location_template_directory]),lstrip_blocks = True,trim_blocks=True)
+	env = Environment(loader=FileSystemLoader([get_hardware_vendor_template_directory,get_location_template_directory]),lstrip_blocks = True,trim_blocks=True)
 	baseline = env.get_template(template)
 	os.makedirs('{}/rendered-configs/'.format(get_real_path()),exist_ok=True)
 	f = open("{}/rendered-configs/{}.{}".format(get_real_path(),node_object[index]['name'],template.replace('jinja2','')) + "conf", "w") 
